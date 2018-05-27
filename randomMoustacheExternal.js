f8fdb83f32a060ed1da37bcd06cfa8d79ccf40d0
Evan Genest <genest.1@osu.edu>
2018-05-27 15:46:53 -0500

    Make the output describe random # calories in # number of Cheetos.  Do this by calling your custom remote number generator inside of ${} syntax.

--- randomMoustacheExternal.js
+++ randomMoustacheExternal.js
@@ -2,5 +2,11 @@ const express = require('express');
 const logger = require('morgan');
 const app = express();
 const blah = require("./js/randy.js");
+const PORT = 5151;
 
 console.log(blah() );
+app.get('/', (req, res)=>{
+	res.send(`There are ${blah()*13} calories in ${blah()}  Cheetos!`);
+} );
+app.listen(PORT);
+console.log(`App is listening on ${PORT}`);
