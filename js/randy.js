9033b44b3b63d9dd2481df6df666f5f2122faaee
Evan Genest <genest.1@osu.edu>
2018-05-27 15:05:42 -0500

    1st Commit.

---
+++ js/randy.js
@@ -0,0 +1,6 @@
+// May 27, 2018
+
+const MAX = 20;
+
+const  = 
+return Math.floor( Math.random()*MAX);
---
+++ randomMoustacheExternal.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const logger = require('morgan');
+const app = express();
+const randy = require("./js/randy");
